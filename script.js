a14f95e77e37def57b8eded1ce79b15315d91090
Ejona Shala <ejona.shala@universitetiaab.com>
2025-11-25 22:27:08 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if(bar){
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+
+    })
+}
+
+if(close){
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+
+    })
+}
+
+function toggleForms() {
+    const loginForm = document.getElementById('login-form');
+    const registerForm = document.getElementById('register-form');
+
+    if (loginForm.style.display === 'none') {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+    } else {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+    }
+}
+
